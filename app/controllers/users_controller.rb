9e56fb2498bd20e05c3175f8d96e93c85a076d0f
Alex Cheung <alex@juscollege.com>
2014-05-24 20:13:04 -0700

    update files for faster reusability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
       sign_in @user
-  		flash[:success] = "Welcome to DrillIt!"
+  		flash[:success] = "Welcome to Provence Inspiration!"
   		redirect_to @user
   	else
   		render 'new'
@@ -51,23 +51,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
-
-    # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
     end
 end
